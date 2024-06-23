79594ee0e81e21f742753044f006d31ce61fdac4
Khan9Tran <trankhangtctv@gmail.com>
2024-06-23 13:43:49 +0700

    thêm route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.scss'
 import Header from './components/Header/Header'
-
+import { Link } from 'react-router-dom'
 function App() {
   return (
     <div className="App-container">
       <Header />
+      <div>
+        TEST LINK
+      </div>
+      <div>
+        <button>
+          <Link to={'/users'}>User page</Link>
+        </button>
+        <button>
+          <Link to={'/admins'}>Admin page</Link>
+        </button>
+      </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,24 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Admin from './components/Admin/Admin.jsx'
+import User from './components/User/User.jsx'
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     {/* <React.StrictMode> */}
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="users" element={<User />} />
+          <Route path="admins" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
     {/* </React.StrictMode> */}
   </Provider>
 )
