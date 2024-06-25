8165ba13f16213347b815ebf1e369a4c7c732ce4
Khan9Tran <trankhangtctv@gmail.com>
2024-06-26 00:44:33 +0700

    design home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,12 +19,8 @@ const Header = () => {
             <NavLink to={"/admins"} className='nav-link'>Admin</NavLink>
           </Nav>
           <Nav>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item>Log in</NavDropdown.Item>
-                <NavDropdown.Item>Log out</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item>Profile</NavDropdown.Item>
-                </NavDropdown>
+            <button className='btn-login'>Log in</button>
+            <button className='btn-signup'>Sign up</button>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -8,6 +8,26 @@ const HomePage = () => {
                     type="video/mp4"
                 />
             </video>
+            <div className="homepage-content">
+                <div className='title-1'>
+                    <pre>
+                        {`Make forms
+worth filling out`}
+                    </pre>
+                </div>
+                <div className='title-2'>
+                    <pre>
+                        {
+                    `Get more data—like signups, feedback, and anything
+else—with forms designed to be refreshingly different.`}
+                    </pre>
+                </div>
+                <div className='title-3'>
+                    <button className='btn-getStarted'>
+                        Get started-it's free
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
